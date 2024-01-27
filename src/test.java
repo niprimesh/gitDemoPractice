be4c610c3f520cfbc251de5768da0f786fb608b6
niprimesh <147188559+niprimesh@users.noreply.github.com>
2024-01-27 15:11:45 +0000

    Update test.java

--- src/test.java
+++ src/test.java
@@ -2,5 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("nimesh");
         System.out.println("patel");
+        System.out.println("Prime Testing")
+        
     }
 }
