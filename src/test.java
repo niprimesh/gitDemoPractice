6e33ef8cb45c2d53a23f0d8518e4e7249998cd1a
niprimesh <147188559+niprimesh@users.noreply.github.com>
2024-01-27 15:14:42 +0000

    Update test.java

--- src/test.java
+++ src/test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("nimesh");
+        System.out.println("suresh");
         System.out.println("patel");
     }
 }
